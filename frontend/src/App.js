c5fc148dcb33c078f2fdfcd055311e00eb23bba0
deanadriannn <deanadrian644@gmail.com>
2024-10-06 08:29:02 +0700

    update interface dan laman akses

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes, useLocation } from 'react-router-dom';
 import Sidebar from './Sidebar';
 import Footer from './Footer';
 import Countries from './pages/Countries';
@@ -10,26 +10,142 @@ import DramaInput from './pages/DramaInput';
 import DramaApproved from './pages/DramaApproved';
 import ValidateDrama from './pages/ValidateDrama';
 import HomePage from './pages/HomePage';
+import Login from './pages/Login';
+import Registration from './pages/Registration';
+import MovieDetail from './pages/MovieDetail';
+import SearchResults from './pages/SearchResults';
 import Users from './pages/Users';
-import './styles.css'; // Pastikan untuk mengimpor CSS
+import './styles.css';
+
+
+function LayoutWithSidebar({ children }) {
+    const location = useLocation();
+
+    // Hide sidebar for specific routes
+    const hideSidebarOn = ['/login', '/registration', '/movie', '/search'];
+    const shouldHideSidebar = hideSidebarOn.some(path => location.pathname.startsWith(path));
+
+    return (
+        <div className="App">
+            {/* Render Sidebar only when it's not hidden */}
+            {!shouldHideSidebar && <Sidebar />}
+            <div className="content">{children}</div>
+            <Footer />
+        </div>
+    );
+}
 
 function App() {
     return (
         <Router>
             <div className="App">
-                <Sidebar />
-                <div className="content">
                     <Routes>
-                        <Route path="/countries" element={<Countries />} />
-                        <Route path="/genres" element={<Genres />} />
-                        <Route path="/actors" element={<Actors />} />
-                        <Route path="/comments" element={<Comments />} />
-                        <Route path="/input-drama" element={<DramaInput />} />
-                        <Route path="/validate-drama" element={<ValidateDrama />} />
-                        <Route path="/approved-drama" element={<DramaApproved />} />
-                        <Route path="/users" element={<Users />} />
+                        <Route
+                            path="/"
+                            element={
+                                <LayoutWithSidebar>
+                                    <HomePage />
+                                </LayoutWithSidebar>
+                            }
+                        />
+                        <Route
+                            path="/login"
+                            element={
+                                <LayoutWithSidebar>
+                                    <Login />
+                                </LayoutWithSidebar>
+                            }
+                        />
+                        <Route
+                            path="/registration"
+                            element={
+                                <LayoutWithSidebar>
+                                    <Registration />
+                                </LayoutWithSidebar>
+                            }
+                        />
+                        <Route
+                            path="/movie/:movieId"
+                            element={
+                                <LayoutWithSidebar>
+                                    <MovieDetail />
+                                </LayoutWithSidebar>
+                            }
+                        />
+                        <Route
+                            path="/search/:searchTerm"
+                            element={
+                                <LayoutWithSidebar>
+                                    <SearchResults />
+                                </LayoutWithSidebar>
+                            }
+                        />
+                        {/* Routes for pages where sidebar should be visible */}
+                        <Route
+                            path="/countries"
+                            element={
+                                <LayoutWithSidebar>
+                                    <Countries />
+                                </LayoutWithSidebar>
+                            }
+                        />
+                        <Route
+                            path="/genres"
+                            element={
+                                <LayoutWithSidebar>
+                                    <Genres />
+                                </LayoutWithSidebar>
+                            }
+                        />
+                        <Route
+                            path="/actors"
+                            element={
+                                <LayoutWithSidebar>
+                                    <Actors />
+                                </LayoutWithSidebar>
+                            }
+                        />
+                        <Route
+                            path="/comments"
+                            element={
+                                <LayoutWithSidebar>
+                                    <Comments />
+                                </LayoutWithSidebar>
+                            }
+                        />
+                        <Route
+                            path="/input-drama"
+                            element={
+                                <LayoutWithSidebar>
+                                    <DramaInput />
+                                </LayoutWithSidebar>
+                            }
+                        />
+                        <Route
+                            path="/validate-drama"
+                            element={
+                                <LayoutWithSidebar>
+                                    <ValidateDrama />
+                                </LayoutWithSidebar>
+                            }
+                        />
+                        <Route
+                            path="/approved-drama"
+                            element={
+                                <LayoutWithSidebar>
+                                    <DramaApproved />
+                                </LayoutWithSidebar>
+                            }
+                        />
+                        <Route
+                            path="/users"
+                            element={
+                                <LayoutWithSidebar>
+                                    <Users />
+                                </LayoutWithSidebar>
+                            }
+                        />
                     </Routes>
-                </div>
                 <Footer />
             </div>
         </Router>
