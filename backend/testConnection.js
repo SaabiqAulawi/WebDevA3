4c0af9e72b0476f7cf140e8a4ef6698f88e4fd08
aulawisaabiq <aulawisaabiq@gmail.com>
2024-10-06 11:15:54 +0700

    Update Backend

---
+++ backend/testConnection.js
@@ -0,0 +1,15 @@
+const sequelize = require('./config/database'); // Sesuaikan dengan path file Anda
+const Country = require('./models/Country'); // Sesuaikan dengan path file Anda
+
+// Menguji koneksi ke database dan sinkronisasi model
+sequelize.authenticate()
+  .then(() => {
+    console.log('Connection has been established successfully.');
+    return Country.sync(); // Untuk membuat tabel jika belum ada
+  })
+  .then(() => {
+    console.log('Country model has been synchronized with the database.');
+  })
+  .catch(err => {
+    console.error('Unable to connect to the database:', err);
+  });
